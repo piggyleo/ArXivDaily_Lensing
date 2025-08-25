e4f8f1813f11e428bbf568a9d69081c8bcb67e0a
piggyleo <143366809+piggyleo@users.noreply.github.com>
2025-08-25 13:12:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = 'olozhika'
+USERNAME = 'piggyleo'
 
 # The repository to add this issue to
-REPO_OWNER = 'olozhika'
-REPO_NAME = 'ArXivDaily_StarFormation'
+REPO_OWNER = 'piggyleo'
+REPO_NAME = 'ArXivDaily_Lensing'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["star formation", "star-forming", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF"]
+KEYWORD_LIST = ["gravitational lensing", "strong lensing", "dark matter", "machine learning", "neural network"]
 # Keywords to exclude
-KEYWORD_EX_LIST = ["galaxies", "galaxy cluster", " AGN ", "standard candle", "X-ray binar", "solar corona"]
+KEYWORD_EX_LIST = []
 # Note that the 'Keywords' above are actually searched in the abstract instead of the real keyword section. 
